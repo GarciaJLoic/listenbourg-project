53773c21b51c956001c96fc2646a2e1688cae4e3
JeanLoic GARCIA <(mailto:garcia.jeanloic@gmail.com)>
2022-12-04 11:33:49 +0100

    Fix bugs and add new icons in navBar

--- main.js
+++ main.js
@@ -11,6 +11,7 @@ function navMenuExpand() {
     elementsMenu[elementMenuArray[i]].classList.toggle('hidden'); /* Si les éléments n'ont pas la classe hidden, l'ajoute sinon la retire */
 
     if (elementsMenu[elementMenuArray[i]].classList[0] === "none") { /* Vérifie si les éléments ont la classe none */
+
         for (let j = 0; j < elementMenuArray.length; j++) { /*  Boucle qui recommence pour tous les éléments*/
             elementsMenu[elementMenuArray[j]].classList.remove('none'); /*  Retire la class none à tous les éléments*/
         }
@@ -21,20 +22,23 @@ function navMenuExpand() {
         setTimeout(navMenuExpand, 100) //Attend 1s avant de relancer la fonction.
     }
     else { // S'il ne reste plus d'élément 
-        document.querySelector("#navButton").disabled = false; // Réactive le bouton
         elementMenuArray = elementMenuArray.reverse(); // Inverse le tableau des éléments
         i = 0; // Remet la boucle à zéro
         if (elementsMenu[elementMenuArray[i]].classList[0] === "hidden") { /* Vérifie si les éléments ont la classe hidden */
             setTimeout(function () { /*  Paramètre le délai avant de lancer la boucle*/
                 for (let i = 0; i < elementMenuArray.length; i++) {/*  Boucle qui recommence pour tous les éléments*/
                     elementsMenu[elementMenuArray[i]].classList.add('none');/*  Ajoute la class none à tous les éléments*/
+
                 }
-            }, 200)
+            }, 500)
         }
     }
 }
 
 buttonMenu.onclick = function () { // Active la fonction lors du clic du bouton
     document.querySelector("#navButton").disabled = true; // Désactive le bouton
+    setTimeout(function () {
+        document.querySelector("#navButton").disabled = false; // Réactive le bouton
+    }, 1000)
     navMenuExpand(); // Lance la fonction 
 };
