62f8acba8d331b5759c041e672e3c1be65c4f1a2
Noiramd31 <marion2macedo@gmail.com>
2022-12-07 15:44:40 +0100

    gallery + modif footer

--- main.js
+++ main.js
@@ -38,3 +38,26 @@ buttonMenu.onclick = function () { // Active la fonction lors du clic du bouton
     document.querySelector("#navButton").disabled = true; // Désactive le bouton
     navMenuExpand(); // Lance la fonction 
 };
+
+/* ******************************* */
+/*   GALERY*/
+/* *****************************/
+let allGridItems = [...document.getElementsByClassName("grid-item")];
+let popupBg = document.getElementById("popup-bg");
+let popupImg = document.getElementById("popup-img");
+
+const openPopup = (e) => {
+    let gridItemClicked = e.target.closest(".grid-item");
+    let clickedImageName = gridItemClicked.id;
+    popupBg.classList.add("active");
+    popupImg.src = `./galery/${clickedImageName}.jpg`;
+};
+
+const closePopup = () => {
+    popupBg.classList.remove("active");
+};
+
+allGridItems.forEach((el) => el.addEventListener("click", openPopup));
+
+popupImg.addEventListener("click", (e) => e.stopPropagation());
+popupBg.addEventListener("click", closePopup);
